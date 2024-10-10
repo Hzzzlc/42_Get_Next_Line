31714813b0561ca0407a34b0c6f6712df78556ef
hazel <hazelnicolecm@gmail.com>
2024-10-10 16:42:00 +0200

    apuntes get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hcruz-me <hcruz-me@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 12:49:51 by hcruz-me          #+#    #+#             */
-/*   Updated: 2024/10/09 13:10:06 by hcruz-me         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:39:50 by hcruz-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*get_next_line(int fd)
 {
-	fd = open("hazel.txt", O_RDONLY);
+	fd = open("hazel.txt", O_RDWR);
 }
+char *set_line(char *buffer)
+{
+	int fd;
+	ssize_t bytes_read;
+	fd = open("hazel.txt", O_RDONLY);
+	//Hay que leer el archivo hasta que encuentre un
+	//un salgo de linea esto se hara con strchr.
+	//esto a su vez generara un buffer que sea del tamano
+	//de lo que ha leido.
+}
+//Tendremos otra funcion que rellenara el buffer a la linea
+//que copio, la cual se le pasara como parametro fd, char lesftc
+//char buffer. utilizaremos tambien BUFFER_SIZE, hasta el salto de
+//linea o terminador.
